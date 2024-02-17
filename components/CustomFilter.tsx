957c5f37eb7288b5919aa98e9b043f4673b695b5
torio <raikan327@icloud.com>
2024-02-18 08:30:39 +0900

    initial commit

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, {Fragment, useState} from 'react';
+import {CustomFilterProps} from "@/types";
+import {Listbox, Transition} from "@headlessui/react";
+import Image from "next/image";
+import {router} from "next/client";
+import {useRouter} from "next/navigation";
+import {updateSearchParams} from "@/utils";
+
+function CustomFilter({title,options,setFilter}:CustomFilterProps) {
+    const router=useRouter()
+    const [selected,setSelected]=useState(options[0]);
+
+
+    return (
+        <div className={'w-fit'}>
+            <Listbox
+            value={selected}
+            onChange={(e)=>{setSelected(e),setFilter(e.value)}}>
+                <div className="relative w-fit z-10">
+                    <Listbox.Button className={'custom-filter__btn'}>
+                        <span className={'block truncate'}>{selected.title}</span>
+                        <Image src={'/chevron-up-down.svg'} alt={'chevron up down'} width={20} height={20}/>
+                    </Listbox.Button>
+                    <Transition as={Fragment} leave={'transition ease-in duration-100'}
+                    leaveFrom={'opacity-100'}
+                    leaveTo={'opacity-0'}>
+                        <Listbox.Options className={'custom-filter__options'}>
+                            {options.map((option)=>(
+                                <Listbox.Option key={option.title} value={option}
+                                className={({active})=>`relative cursor-default select-none py-2 px-4 ${active? 'bg-primary-blue text-white':'text-gray-900'}`}>
+                                    {({selected})=>(
+                                        <span className={`block truncate ${selected ?'font-medium':'font-normal'}`}>
+                                            {option.title}
+                                        </span>
+                                    )}
+                                </Listbox.Option>
+                            ))}
+                        </Listbox.Options>
+                    </Transition>
+                </div>
+            </Listbox>
+        </div>
+    );
+}
+
+export default CustomFilter;
---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from "@/components/Hero";
+import Navber from "@/components/Navber";
+import Footer from "@/components/Footer";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import CustomButton from "@/components/CustomButton";
+import SearchManufacturer from "@/components/SearchManufacturer";
+import CarCard from "@/components/CarCard";
+import CarDetails from "@/components/CarCard";
+import ShowMore from "@/components/ShowMore";
+
+export { Hero,
+    CustomButton,
+    Navber,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore,
+};
